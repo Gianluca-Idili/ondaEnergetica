31aec153d4ceb3429fa94fbe8658ed1c5b4ac86b
Blasta 94 <gianlucaidili94@gmail.com>
2023-12-20 13:25:49 +0100

    scheletro apparentemente concluso

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     });
 });
+
+function emailSend(){
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "gianlucaidili1@gmail.com",
+        Password : "DF8673A72A2407FC5EB20346BBC7C544A824",
+        To : 'gianlucaidili94@gmail.com',
+        From : "gianlucaidili1@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+
+}
